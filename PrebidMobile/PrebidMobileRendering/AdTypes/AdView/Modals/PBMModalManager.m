71a1de1d1baaf298ac53d0c54023f09550705c45
James Lou <jlou@yahooinc.com>
2025-04-22 17:22:45 -0700

    Migrate PBMDeferredModalState interfaces to swift

--- PrebidMobile/PrebidMobileRendering/AdTypes/AdView/Modals/PBMModalManager.m
+++ PrebidMobile/PrebidMobileRendering/AdTypes/AdView/Modals/PBMModalManager.m
@@ -24,7 +24,6 @@
 #import "PBMNonModalViewController.h"
 #import "PBMModalViewControllerDelegate.h"
 #import "PBMModalState.h"
-#import "PBMDeferredModalState.h"
 #import "PBMMacros.h"
 #import "PBMModalAnimator.h"
 
@@ -46,7 +45,7 @@ static NSString * const PBMInterstitialStoryboardName  = @"Interstitial";
 @property (nonatomic,weak,nullable) id<PBMModalManagerDelegate> delegate;
 
 @property (nonatomic,assign,readwrite) BOOL isModalDismissing;
-@property (nonatomic,strong,nullable) PBMDeferredModalState *deferredModalState;
+@property (nonatomic,strong,nullable) id<PBMDeferredModalState> deferredModalState;
 
 //The last item in this stack represents the view & display properties currently being displayed.
 @property (nonatomic, strong, nonnull) NSMutableArray<id<PBMModalState>> *modalStateStack;
@@ -142,11 +141,11 @@ static NSString * const PBMInterstitialStoryboardName  = @"Interstitial";
     return [self removeStateBlock:state];
 }
 
-- (void)pushDeferredModal:(nonnull PBMDeferredModalState *)deferredModalState {
+- (void)pushDeferredModal:(nonnull id<PBMDeferredModalState>)deferredModalState {
     if (self.deferredModalState == nil && !self.isModalDismissing) {
         self.deferredModalState = deferredModalState;
         @weakify(self);
-        __weak PBMDeferredModalState *weakDeferredState = deferredModalState;
+        __weak id<PBMDeferredModalState> weakDeferredState = deferredModalState;
         [deferredModalState prepareAndPushWithModalManager:self discardBlock:^{
             @strongify(self);
             if (!self) { return; }
